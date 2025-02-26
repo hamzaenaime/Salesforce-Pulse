280a7e8d0d1fefdcb6cecb4b58db29d936537e3a
Hamza.Enaime <hamza.enaime@britishgas.co.uk>
2025-02-26 13:52:02 +0000

    Initial Commit

---
+++ Salesforce Pulse/Salesforce_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salesforce_PulseApp.swift
+//  Salesforce Pulse
+//
+//  Created by Hamza Enaime on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Salesforce_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Salesforce PulseTests/Salesforce_PulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Salesforce_PulseTests.swift
+//  Salesforce PulseTests
+//
+//  Created by Hamza Enaime on 2/26/25.
+//
+
+import Testing
+@testable import Salesforce_Pulse
+
+struct Salesforce_PulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
